12ca0fe8098c374c4dbe16a3d9ea38cb0add7488
Douglas Gonçalves <douglas.goncalves@grupofolha.com.br>
2020-09-09 14:02:06 -0300

    Inicio do sistema

---
+++ src/admin/controllers/productsUpdate.php
@@ -0,0 +1,46 @@
+<?php
+
+    namespace psiu\admin\controllers;
+
+    use driver\control\action;
+    use data\resource\resource;
+    use psiu\common\models\users;
+
+    class productsUpdate extends action
+    {
+        const _LOCAL = __DIR__;
+
+        /**
+         * Função a ser executada no contexto da action
+         *
+         * @param array $info
+         * @return void
+         */
+        public function main(array $info)
+        {
+            $params = $info['url'];
+            return $this->view($params);
+        }
+
+        /**
+         * Para ser disparado antes
+         *
+         * @return void
+         */
+        public function _before()
+        {
+
+        }
+
+        /**
+         * Para ser disparado depois
+         *
+         * @return void
+         */
+        public function _after()
+        {
+
+        }
+    }
+
+?>
